e172259d51f045ef406fb60c1c264739f8fb1f4e
abdul rafay <abdulrafay1747@gmail.com>
2026-01-21 01:01:14 +0500

    test

--- test-code.js
+++ test-code.js
@@ -1,19 +1,13 @@
+app.get('/user', (req, res) => {
+  const userId = req.query.id;
+  const query = `SELECT * FROM users WHERE id = ${userId}`;
+  db.query(query, (err, result) => {
+    res.json(result);
+  });
+});
+
 app.get('/fetch', async (req, res) => {
   const url = req.query.url;
   const response = await axios.get(url);
   res.json(response.data);
-});
-
-app.post('/webhook-proxy', async (req, res) => {
-  const { targetUrl, payload } = req.body;
-  await axios.post(targetUrl, payload);
-  res.send('Forwarded');
-});
-
-app.post('/search', (req, res) => {
-  const { username, email } = req.body;
-  const sql = `SELECT * FROM users WHERE username = '${username}' OR email = '${email}'`;
-  db.query(sql, (err, results) => {
-    res.json(results);
-  });
 });
