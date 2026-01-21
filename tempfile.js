4a13603f5eef7efbcddb7ca21ab09ae50deba419
abdul rafay <abdulrafay1747@gmail.com>
2026-01-22 03:31:55 +0500

    fixed

--- tempfile.js
+++ tempfile.js
@@ -1,5 +1,13 @@
+function escapeHtml(str) {
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
 app.get('/profile', (req, res) => {
-  const name = req.query.name;
-  // Vulnerable: reflected XSS
+  const name = escapeHtml(req.query.name || '');
   res.send(`<html><body><h1>Welcome, ${name}!</h1></body></html>`);
 });
